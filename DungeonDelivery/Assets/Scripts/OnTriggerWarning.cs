c916607bdc6c2751d268f8df15e7004456ef7e45
RikLangeveld <Rik_langeveld@hotmail.com>
2018-09-11 15:05:44 +0200

    meer shit

--- DungeonDelivery/Assets/Scripts/OnTriggerWarning.cs
+++ DungeonDelivery/Assets/Scripts/OnTriggerWarning.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class OnTriggerWarning : MonoBehaviour
 {
+    [SerializeField]
     GameObject textToDisplay;
+
+    [SerializeField]
+    GameObject camera;
+
+    [SerializeField]
+    float displacementAmount = 0.95f;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "player")
+        if (other.tag == "Player")
         {
             PlayerPizzaController pizzaManager = other.GetComponent<PlayerPizzaController>();
             if (pizzaManager.pizza < 1)
@@ -19,9 +27,15 @@ public class OnTriggerWarning : MonoBehaviour
 
     IEnumerator TellPlayerToPickPizza(Collider2D other)
     {
+        PlayerMovement playerMovement = other.GetComponent<PlayerMovement>();
+
         textToDisplay.SetActive(true);
-        other.GetComponent<PlayerMovement>().isActiveAndEnabled(false)
+        other.GetComponent<PlayerMovement>().enabled = false;
+        camera.GetComponent<Animation>().Play();
         yield return new WaitForSeconds(3);
+        other.GetComponent<PlayerMovement>().enabled = true;
+        textToDisplay.SetActive(false);
+        Destroy(this);
     }
 
 }
