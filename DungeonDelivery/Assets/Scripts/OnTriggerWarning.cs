b708c1d1a5708f5ac2913be0c6071198bf535f51
Unknown <Ricardo-42-ajax@live.nl>
2018-09-11 15:57:39 +0200

    forgetPizzaScene

--- DungeonDelivery/Assets/Scripts/OnTriggerWarning.cs
+++ DungeonDelivery/Assets/Scripts/OnTriggerWarning.cs
@@ -31,11 +31,39 @@ public class OnTriggerWarning : MonoBehaviour
 
         textToDisplay.SetActive(true);
         other.GetComponent<PlayerMovement>().enabled = false;
-        camera.GetComponent<Animation>().Play();
+        other.GetComponent<Animator>().Play("ForgetPizzaScene");
         yield return new WaitForSeconds(3);
+
+        other.transform.localScale = new Vector2(Mathf.Abs(other.transform.localScale.x), other.transform.localScale.y);
+
+        float timer = 0;
+        float duration = 0.4f;
+
+        Debug.Log("1");
+
+        while (timer <= duration)
+        {
+            other.transform.position = new Vector2(other.transform.position.x - 0.4f * Time.deltaTime, other.transform.position.y);
+            timer += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+
+        /*
+        float timer = 0.001f;
+        float totalDuration = 5;
+        float startPosX = other.transform.position.x;
+        float goalPosX = other.transform.position.x - 0.2f;
+        while (timer <= totalDuration)
+        {
+            float lerp = Mathf.Lerp(startPosX, goalPosX, timer / totalDuration);
+            timer += Time.deltaTime;
+            other.transform.position = new Vector2(lerp, other.transform.position.y);
+        }
+        */
+
         other.GetComponent<PlayerMovement>().enabled = true;
         textToDisplay.SetActive(false);
-        Destroy(this);
+        //Destroy(this);
     }
 
 }
