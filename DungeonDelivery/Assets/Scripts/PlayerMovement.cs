30b3f4fb431a9170883ff86f7d3863aa6b8751ef
Unknown <Ricardo-42-ajax@live.nl>
2018-09-11 13:38:52 +0200

    Changed movement to rigidbody

--- DungeonDelivery/Assets/Scripts/PlayerMovement.cs
+++ DungeonDelivery/Assets/Scripts/PlayerMovement.cs
@@ -7,38 +7,53 @@ public class PlayerMovement : MonoBehaviour {
     [SerializeField]
     private float speed = 5;
 
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     private void Update()
     {
         Vector3 tempPos = transform.position;
         Vector3 tempScale = transform.localScale;
 
+        float moveX = rb.position.x;
+        float moveY = rb.position.y;
+
         // Up
         if (Input.GetKey(KeyCode.W))
         {
-            tempPos.y += speed * Time.deltaTime;
+            //tempPos.y += speed * Time.deltaTime;
+            moveY += speed * Time.deltaTime;
         }
 
         // Down
         if (Input.GetKey(KeyCode.S))
         {
-            tempPos.y -= speed * Time.deltaTime;
+            //tempPos.y -= speed * Time.deltaTime;
+            moveY -= speed * Time.deltaTime;
         }
 
         // Left
         if (Input.GetKey(KeyCode.A))
         {
             tempScale.x = Mathf.Abs(tempScale.x);
-            tempPos.x -= speed * Time.deltaTime;
+            //tempPos.x -= speed * Time.deltaTime;
+            moveX -= speed * Time.deltaTime;
         }
 
         // Right
         if (Input.GetKey(KeyCode.D))
         {
             tempScale.x = Mathf.Abs(tempScale.x) * -1;
-            tempPos.x += speed * Time.deltaTime;
+            //tempPos.x += speed * Time.deltaTime;
+            moveX += speed * Time.deltaTime;
         }
 
-        transform.position = tempPos;
+        rb.MovePosition(new Vector2(moveX, moveY));
+        //transform.position = tempPos;
         transform.localScale = tempScale;
     }
 }
