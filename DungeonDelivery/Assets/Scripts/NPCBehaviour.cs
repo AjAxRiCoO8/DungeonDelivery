9f0d5815f529737496b1afb77ee71aa3c08ee7f3
K van der Lans <kayvanderlans@gmail.com>
2018-09-11 15:09:44 +0200

    stuf

---
+++ DungeonDelivery/Assets/Scripts/NPCBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class NPCBehaviour : MonoBehaviour
+{
+    [SerializeField] private GameObject _SpeechBubble;
+    private bool _GotPizzaDelivered;
+
+    private void Start()
+    {
+        _GotPizzaDelivered = false;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && Input.GetKeyDown(KeyCode.F) && !_GotPizzaDelivered)
+        {
+            _GotPizzaDelivered = true;
+            StartCoroutine(ShowMessage());
+        }
+    }
+
+    private IEnumerator ShowMessage()
+    {
+        yield return new WaitForSeconds(3);
+    }
+}
