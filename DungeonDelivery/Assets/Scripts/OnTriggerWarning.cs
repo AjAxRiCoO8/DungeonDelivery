50437026ea4081d105844d728e8593e1a3e483be
RikLangeveld <Rik_langeveld@hotmail.com>
2018-09-11 15:05:36 +0200

    shit

---
+++ DungeonDelivery/Assets/Scripts/OnTriggerWarning.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnTriggerWarning : MonoBehaviour
+{
+    GameObject textToDisplay;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "player")
+        {
+            PlayerPizzaController pizzaManager = other.GetComponent<PlayerPizzaController>();
+            if (pizzaManager.pizza < 1)
+            {
+                StartCoroutine(TellPlayerToPickPizza(other));
+            }
+        }
+    }
+
+    IEnumerator TellPlayerToPickPizza(Collider2D other)
+    {
+        textToDisplay.SetActive(true);
+        other.GetComponent<PlayerMovement>().isActiveAndEnabled(false)
+        yield return new WaitForSeconds(3);
+    }
+
+}
